4e9fc9c5a86da508fb6ecb5c79014bd08554b9a3
Nguyen Thi Ly <nguyenthilyctt11cd3a@gmail.com>
2020-03-11 11:02:18 +0700

    Add project files.

---
+++ connection/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace connection
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+
+            {
+
+                using (SqlConnection connection =
+
+                    new SqlConnection(@"Server=PC-11\SQLEXPRESS;
+ 
+  Database=quanlysinhvien123; Integrated Security=SSPI"))
+
+                {
+
+                    connection.Open();
+
+                }
+
+                MessageBox.Show("Mo va dong co so du lieu thanh cong.");
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                MessageBox.Show("Loi khi mo  ket noi: " + ex.Message);
+
+            }
+        }
+    }
+}
